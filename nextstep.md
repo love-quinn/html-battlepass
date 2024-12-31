b6bffb0133b88c20c6a99f7c38a6403d30ab6c0d
love-quinn <63768468+love-quinn@users.noreply.github.com>
2024-12-31 18:01:40 -0300

    started 2nd page

--- nextstep.md
+++ nextstep.md
@@ -1,5 +1,12 @@
-30-12-2024
+30.12.2024
     [x] Fazer Degisn Basic Card 
     [x] Fazer Degisn Premium Card
     [x] Overflow em caso de multiplos regular cards
-    [] Overflow em caso de multiplos premium cards
+
+31.12.2024
+    [x] Overflow em caso de multiplos premium cards
+    [x] Rederizar ambos cards
+    [x] Scrollar ao pressinao em botões
+    [x] Fazer barra de progresso
+    [x] Adicionar anchor tag para outras páginas
+    [x] Mudar header dinâmicamente para cada página selecionada
--- script.js
+++ script.js
@@ -1,21 +1,29 @@
+
 // script.js
 document.addEventListener("DOMContentLoaded", function() {
     // alert("carreguei")
-    const cardContainer = document.querySelector('.regular-flex');
+    const regularCardsContainer = document.querySelector('#regular-cards-container');
+    const premiumCardsContainer = document.querySelector('#premium-cards-container');
+
+    const cardsRoulette = document.querySelector('#cards-roulette');
+    const leftButton = document.querySelector("#left-button");
+    const rightButton = document.querySelector("#right-button");
+
 
     // Function to create a card
     function createCard(type, level) {
         const cardDiv = document.createElement('div');
         cardDiv.className = type === 'regular' ? 'regular-card flex' : 'premium-card flex';
         cardDiv.innerHTML = `
+          
             <div class="mt-10 flex flex-col items-center gap-3">
                     
                     <div class="bg-zinc-800 p-3 rounded-lg regular-card">
                       <img src="./images/towel.png" alt="">
                     </div>
 
-                    <div class="flex gap-2 items-center">
-                      <i data-lucide="check" height="18" width="18" stroke-width="1.4"></i>
+                    <div class="flex gap-1 items-center">
+                      <img src="./images/check.svg"></i>
                       <p>
                         Level ${level}
                       </p>
@@ -26,13 +34,42 @@ document.addEventListener("DOMContentLoaded", function() {
         return cardDiv;
     }
 
-    // Add 10 regular cards
-    for (let i = 1; i <= 8; i++) {
-        cardContainer.appendChild(createCard('regular', i));
+    function createPremiumCard(level) {
+      const premiumCardDiv = document.createElement('div');
+      premiumCardDiv.className = 'flex';
+      premiumCardDiv.innerHTML = `
+        
+          <div class="mt-10 flex flex-col items-center gap-3">
+                  
+                  <div class="bg-zinc-800 p-3 rounded-md premium-card relative border-2 border-black">
+                    <h2 class="bg-black rounded-lg text-center">
+                      Premium
+                    </h2>
+                    <img src="./images/towel.png" alt="">
+                  </div>
+
+                  <div class="flex gap-1 items-center">
+                    <img src="./images/badge-check.svg"></i>
+                    <p>
+                      Level ${level}
+                    </p>
+                  </div>
+            </div>
+      `;
+      return premiumCardDiv;
     }
 
-    // Add 10 premium cards
-    // for (let i = 1; i <= 10; i++) {
-    //     cardContainer.appendChild(createCard('premium', i));
-    // }
+    // Add 10 regular cards
+    for (let i = 1; i <= 8; i++) {
+        regularCardsContainer.appendChild(createCard('regular', i));
+        premiumCardsContainer.appendChild(createPremiumCard(i));
+    }
+
+    leftButton.addEventListener('click', function() {
+      cardsRoulette.scrollBy({ left: -200, behavior: 'smooth' });
+    });
+
+    rightButton.addEventListener('click', function() {
+      cardsRoulette.scrollBy({ left: 200, behavior: 'smooth' });
+    });
 });
